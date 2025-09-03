a81ee9b27e1eb430adb5ec032b5eb0a4df656f43
Shahzad Ahamd <shahzadcreations16@gmail.com>
2025-09-03 22:06:45 +0500

    Day_02

---
+++ Day_02/second.py
@@ -0,0 +1,31 @@
+# welcome to day two of python progrms
+
+# todayas lesson about primitive datatypes in python
+
+
+# The datatypes are the.
+# 01 .string 
+# 02 .integer 
+# 03 .float 
+# 04 .boolean 
+
+
+
+
+
+# And the second tasks is how to change the datatypes and how to check 
+
+
+
+
+
+# And also in the next lecture about mathemathical opreations like + - * / // 
+
+
+
+# And the next topic about number manipulation
+
+
+
+
+# And the next tasks is tip calcultaor 
